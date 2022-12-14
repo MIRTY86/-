915318a0ac7e604abca8157daf996550656c5e33
Dmitriy Linov <Dimalinov@mail.ru>
2022-12-14 22:32:38 +0300

    Создал пятый проект

---
+++ Project_005_IfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя:");
+string username = Console.ReadLine();
+
+if(username == "Дима")
+{
+    Console.WriteLine("Ура, это же Дима!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
+
+
