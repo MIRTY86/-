56faf9232654ddf28b373209623c3adb7a245048
Dmitriy Linov <Dimalinov@mail.ru>
2022-12-19 15:49:20 +0300

    Задача совсем непонятная

---
+++ Project_011/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] Collection)
+{
+    int length = Collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        Collection[index] = new Random().Next(1, 10);
+
+
+        index++;
+    }
+
+
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+
+    }
+}
+
+int IdexOf(int[] Collection, int find)
+{
+    int count = Collection.Length;
+    int index = 0;
+    int position = 0;
+    while(index < count)
+    {
+        if(Collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IdexOf(array, 4);
+Console.WriteLine(pos);
